6c1b43f6e1d703381594340685918483815e1062
Ananta Podder <31305962+AnantaPodder@users.noreply.github.com>
2021-03-28 18:08:46 +0530

    Update jebot.py

--- jebot.py
+++ jebot.py
@@ -34,6 +34,61 @@ async def start(client, message):
 @Jebot.on_message(filters.text
                    & ~filters.edited
                    & filters.regex(YTDL_REGEX))
+
+#New Edit
+async def callback_query_ytdl_audio(_, message: Message):
+    try:
+        url = message.reply_to_message.text
+        ydl_opts = {
+            'format': 'bestaudio',
+            'outtmpl': '%(title)s - %(extractor)s-%(id)s.%(ext)s',
+            'writethumbnail': True
+        }
+        with YoutubeDL(ydl_opts) as ydl:
+            await message.reply_chat_action("typing")
+            info_dict = ydl.extract_info(url, download=False)
+            # download
+            await message.edit_message_text("**Downloading audio...**")
+            ydl.process_info(info_dict)
+            # upload
+            audio_file = ydl.prepare_filename(info_dict)
+            task = asyncio.create_task(send_audio(message, info_dict,
+                                                  audio_file))
+            while not task.done():
+                await asyncio.sleep(3)
+                await message.reply_chat_action("upload_document")
+            await message.reply_chat_action("cancel")
+            await message.delete()
+    except Exception as e:
+        await message.reply_text(e)
+    await message.reply_to_message.delete()
+    await message.delete()
+
+
+async def send_audio(message: Message, info_dict, audio_file):
+    basename = audio_file.rsplit(".", 1)[-2]
+    # .webm -> .weba
+    if info_dict['ext'] == 'webm':
+        audio_file_weba = basename + ".weba"
+        os.rename(audio_file, audio_file_weba)
+        audio_file = audio_file_weba
+    # thumbnail
+    thumbnail_url = info_dict['thumbnail']
+    thumbnail_file = basename + "." + \
+        get_file_extension_from_url(thumbnail_url)
+    # info (s2tw)
+    webpage_url = info_dict['webpage_url']
+    title = '@Infinity_BOTs - '+s2tw(info_dict['title'])
+    caption = f"<b><a href=\"{webpage_url}\">{title}</a></b>"
+    duration = int(float(info_dict['duration']))
+    performer = s2tw(info_dict['uploader'])
+    await message.reply_audio(audio_file, caption=caption, duration=duration,
+                              performer=performer, title=title,
+                              parse_mode='HTML', thumb=thumbnail_file)
+    os.remove(audio_file)
+    os.remove(thumbnail_file)
+
+#new edit ends
 async def ytdl_with_button(_, message: Message):
     await message.reply_text(
         "**Choose download type 🤗**",
@@ -50,6 +105,7 @@ async def ytdl_with_button(_, message: Message):
         quote=True
     )
 
+      
 
 @Jebot.on_callback_query(filters.regex("^ytdl_audio$"))
 async def callback_query_ytdl_audio(_, callback_query):
